1e24b2bd56eecd20ded5a24df407f0b1b24fa207
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-25 10:58:35 -0400

    chore: remove requirements folder

--- apps/drones/models.py
+++ apps/drones/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Drone(models.Model):
+    serial_number = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    weight_limit = models.IntegerField()
+    battery_capacity = models.IntegerField(default=100)
+    state = models.CharField(max_length=50)
+
+
+class Medication(models.Model):
+    name = models.CharField(max_length=255)
+    weight = models.FloatField()
+    code = models.CharField(max_length=255)
+    image = models.ImageField()
