afc4d8b73e16fb05c3b69be1b6728283e2a45bc7
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-25 12:24:18 -0400

    perf: admin view

--- apps/drones/admin.py
+++ apps/drones/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Drone, Medication
 
 
-admin.site.register(Drone)
-admin.site.register(Medication)
+@admin.register(Drone)
+class DroneAdmin(admin.ModelAdmin):
+    list_display = ("serial_number", "model", "weight_limit", "battery_capacity", "state")
+    list_filter = ("model", "state")
+
+
+@admin.register(Medication)
+class MedicationAdmin(admin.ModelAdmin):
+    list_display = ("name", "weight", "code", "drone_assigned")
+    list_filter = ("drone_assigned",)
