16ba3f0ef8f7db896a9cc3bfb429e323794c423d
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-25 15:59:26 -0400

    perf: show info from drone's medications

--- apps/drones/serializers.py
+++ apps/drones/serializers.py
@@ -3,17 +3,20 @@ from rest_framework import serializers
 from .models import Drone, Medication
 
 
-class DroneSerializer(serializers.ModelSerializer):
-    medications = serializers.PrimaryKeyRelatedField(many=True, queryset=Medication.objects.all())
-
-    class Meta:
-        model = Drone
-        fields = ["id", "serial_number", "model", "weight_limit", "battery_capacity", "state", "medications"]
-
-
 class MedicationSerializer(serializers.ModelSerializer):
     drone = serializers.ReadOnlyField(source='drone_assigned.model')
 
     class Meta:
         model = Medication
         fields = ["id", "name", "weight", "code", "image", "drone"]
+
+
+class DroneSerializer(serializers.ModelSerializer):
+    medications = MedicationSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Drone
+        fields = ["id", "serial_number", "model", "weight_limit", "battery_capacity", "state", "medications"]
