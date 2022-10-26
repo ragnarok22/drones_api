cde8e4693365b4974c9140ddad9b7acd841403f4
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-26 12:54:40 -0400

    perf: add production setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,17 @@
 from .base import *
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 DEBUG = False
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
