2c5778d61c132ee75db13b1c173cedfc4f330afc
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-29 14:43:35 -0400

    perf: databases requests

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,7 @@
 from .base import *
 
 ALLOWED_HOSTS = get_env_variable("ALLOWED_HOSTS").split(" ")
+print(ALLOWED_HOSTS)
 DEBUG = False
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
@@ -13,6 +14,8 @@ DATABASES = {
         'PASSWORD': get_env_variable('DB_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432,
+        'CONN_MAX_AGE': 600,
+        'ATOMIC_REQUESTS': True,
     }
 }
 
@@ -21,8 +24,9 @@ REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
 )
 
 SECURE_HSTS_SECONDS = 518400
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 CSRF_COOKIE_SECURE = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
