2a737e575ed61110aaec3c815c6f0235af127512
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-30 16:12:09 -0400

    feat: add log module to admin

--- apps/logs/admin.py
+++ apps/logs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Log
+
+
+@admin.register(Log)
+class DroneAdmin(admin.ModelAdmin):
+    list_display = ("message", "created_at", "updated_at")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
