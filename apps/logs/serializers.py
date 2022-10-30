a3c3447805f93010ae2502c5c74c4012c74ada9f
Reinier Hernández <sasuke.reinier@gmail.com>
2022-10-30 16:10:17 -0400

    fix: typo in serializers

--- apps/logs/serializers.py
+++ apps/logs/serializers.py
@@ -6,4 +6,4 @@ from .models import Log
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
-        fields = ["id", "message", "create_at", "updated_at"]
+        fields = ["id", "message", "created_at", "updated_at"]
