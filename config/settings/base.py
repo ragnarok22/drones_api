77d1b90833b9cc1a766de42f55baef57908153b5
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-01 11:17:06 -0400

    feat: add jwt for credentials

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,6 +38,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework_simplejwt",
     "django_celery_beat",
 ]
 
@@ -147,6 +148,9 @@ AUTH_USER_MODEL = 'accounts.User'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 # Celery configurations
--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ from apps.drones import views as drones_views
 from apps.logs import views as logs_views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -21,7 +22,9 @@ urlpatterns = [
     path('', core_views.IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
